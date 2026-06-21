4fbd8a437c059da5bd89232a888344d274ca761f
Jason DeGroff <jason.degroff@go.sfcollege.edu>
2026-06-21 00:07:51 -0400

    Update vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000/',
+        target: 'https://localhost:8000/',
         changeOrigin: true,
         secure: false,
       }
